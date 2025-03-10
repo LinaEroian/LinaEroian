df9dde861dc4186ebd741cf5de5bb9b1a3e475b5
LinaEroian <linaeroian94@gmail.com>
2025-03-09 17:31:51 -0700

    new changes

--- Github portfolio/Github portfolio/ContentView.swift
+++ Github portfolio/Github portfolio/ContentView.swift
@@ -1,20 +1,4 @@
-//
-//  ContentView.swift
-//  Github portfolio
-//
-//  Created by Lina on 3/9/25.
-//
-
-import SwiftUI
-
-struct ContentView: View {
-    @Binding var document: Github_portfolioDocument
-
-    var body: some View {
-        TextEditor(text: $document.text)
-    }
-}
-
-#Preview {
-    ContentView(document: .constant(Github_portfolioDocument()))
-}
+[![Header](https://raw.githubusercontent.com/LinaEroian/LinaEroian/refs/heads/main/bug%20pic.webp)]
+           about me
+           
+           Hello! 
